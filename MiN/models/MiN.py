5d9acbf8dd582d4280c697222ecd691a8ec58a1f
Ngahoang2005 <hoangthanhnga2005@gmail.com>
2026-02-21 08:59:01 +0700

    magmax

--- MiN/models/MiN.py
+++ MiN/models/MiN.py
@@ -135,9 +135,9 @@ class MinNet(object):
         self._clear_gpu()
         
         self.run(train_loader)
-        # print("--> Saving MagMax Snapshot for Task 0...")
-        # for j in range(self._network.backbone.layer_num):
-        #     self._network.backbone.noise_maker[j].after_task_training()
+        print("--> Saving MagMax Snapshot for Task 0...")
+        for j in range(self._network.backbone.layer_num):
+            self._network.backbone.noise_maker[j].after_task_training()
         
         self._network.collect_projections(mode='threshold', val=0.95)
         
@@ -208,9 +208,9 @@ class MinNet(object):
 
 
 
-        # print("--> Applying MagMax Merge...")
-        # for j in range(self._network.backbone.layer_num):
-        #     self._network.backbone.noise_maker[j].after_task_training()
+        print("--> Applying MagMax Merge...")
+        for j in range(self._network.backbone.layer_num):
+            self._network.backbone.noise_maker[j].after_task_training()
 
 
             
